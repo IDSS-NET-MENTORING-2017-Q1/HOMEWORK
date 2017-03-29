fde549ab6563d36ab10eee30b2655cd029564e0c
SemenShchedrin <semen_shchedrin@epam.com>
2017-03-29 17:18:01 +0300

    Module #1. Task #1.

---
+++ FirstModule/FirstTask/Tests/TransformerTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq.Expressions;
+
+namespace FirstTask.Tests
+{
+	[TestFixture]
+	public class TransformerTests
+	{
+		[Test]
+		public void VisitAndConvert_TransformsIncreasingByOneToIncrement()
+		{
+			// Arrange
+			Expression<Func<int, int>> sourceExpression = (a) => a + 1;
+
+			// Act
+			var resultExpression = new Transformer().VisitAndConvert(sourceExpression, "Main");
+
+			// Assert
+			Assert.IsNotNull(resultExpression, "Result should not be null!");
+			Assert.IsInstanceOf<LambdaExpression>(resultExpression, "Result should be a lambda expression!");
+			Assert.IsInstanceOf<UnaryExpression>(resultExpression.Body, "Result's body should be an unary expression!");
+			Assert.AreEqual(ExpressionType.Increment, resultExpression.Body.NodeType,
+				"Result's body should be an increment expression!");
+		}
+
+		[Test]
+		public void VisitAndConvert_TransformsDecreasingByOneToDecrement()
+		{
+			// Arrange
+			Expression<Func<int, int>> sourceExpression = (a) => a - 1;
+
+			// Act
+			var resultExpression = new Transformer().VisitAndConvert(sourceExpression, "Main");
+
+			// Assert
+			Assert.IsNotNull(resultExpression, "Result should not be null!");
+			Assert.IsInstanceOf<LambdaExpression>(resultExpression, "Result should be a lambda expression!");
+			Assert.IsInstanceOf<UnaryExpression>(resultExpression.Body, "Result's body should be an unary expression!");
+			Assert.AreEqual(ExpressionType.Decrement, resultExpression.Body.NodeType,
+				"Result's body should be an decrement expression!");
+		}
+
+		[Test]
+		public void VisitAndConvert_DoNothingForOtherAdditions()
+		{
+			// Arrange
+			Expression<Func<int, int>> sourceExpression = (a) => a + 5;
+
+			// Act
+			var resultExpression = new Transformer().VisitAndConvert(sourceExpression, "Main");
+
+			// Assert
+			Assert.IsNotNull(resultExpression, "Result should not be null!");
+			Assert.IsInstanceOf<LambdaExpression>(resultExpression, "Result should be a lambda expression!");
+			Assert.IsInstanceOf<BinaryExpression>(resultExpression.Body, "Result's body should be a binary expression!");
+			Assert.AreEqual(ExpressionType.Add, resultExpression.Body.NodeType,
+				"Result's body should be an addition expression!");
+		}
+
+		[Test]
+		public void VisitAndConvert_DoNothingForOtherSubstractions()
+		{
+			// Arrange
+			Expression<Func<int, int>> sourceExpression = (a) => a - 5;
+
+			// Act
+			var resultExpression = new Transformer().VisitAndConvert(sourceExpression, "Main");
+
+			// Assert
+			Assert.IsNotNull(resultExpression, "Result should not be null!");
+			Assert.IsInstanceOf<LambdaExpression>(resultExpression, "Result should be a lambda expression!");
+			Assert.IsInstanceOf<BinaryExpression>(resultExpression.Body, "Result's body should be a binary expression!");
+			Assert.AreEqual(ExpressionType.Subtract, resultExpression.Body.NodeType,
+				"Result's body should be a substraction expression!");
+		}
+	}
+}
---
+++ FirstModule/FirstTask/Transformer.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace FirstTask
+{
+	public class Transformer : ExpressionVisitor
+	{
+		protected override Expression VisitBinary(BinaryExpression node)
+		{
+			if (node.Left.NodeType != ExpressionType.Parameter || node.Right.NodeType != ExpressionType.Constant ||
+			    !((ConstantExpression) node.Right).Value.Equals(1))
+				return base.VisitBinary(node);
+
+
+			switch (node.NodeType)
+			{
+				case ExpressionType.Add:
+					return Expression.Increment(node.Left);
+				case ExpressionType.Subtract:
+					return Expression.Decrement(node.Left);
+			}
+
+			return base.VisitBinary(node);
+		}
+	}
+}
