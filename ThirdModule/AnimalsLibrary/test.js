bbeb5b3b769f9366f1c2e5b38955be23fabc46c2
SemenShchedrin <semen_shchedrin@epam.com>
2017-04-11 15:04:46 +0300

    Module #3. Task #3.

---
+++ ThirdModule/AnimalsLibrary/test.js
@@ -0,0 +1,16 @@
+var dog = new ActiveXObject("AnimalsLibrary.Dog");
+
+WScript.Echo(dog.GetVoice());
+WScript.Echo(dog.GetSatiety());
+WScript.Echo(dog.GetHealth());
+
+WScript.Sleep(10000);
+
+WScript.Echo(dog.GetSatiety());
+WScript.Echo(dog.GetHealth());
+
+dog.Feed();
+dog.Heal();
+
+WScript.Echo(dog.GetSatiety());
+WScript.Echo(dog.GetHealth());
