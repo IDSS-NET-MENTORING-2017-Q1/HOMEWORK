ee82463630580f65d82189a68877b4a6dc565fb1
Щедрин Семен Сергеевич <schesswork@gmail.com>
2017-05-04 23:01:49 +0300

    Module #5. Task #1.

--- FifthModule/ScannerManager/Program.cs
+++ FifthModule/ScannerManager/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FifthModule.Classes;
 using NLog;
 using NLog.Config;
